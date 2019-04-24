30c15b830ffc864bb486ee585ddda4592abc82df
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-24 17:22:48 +0100

    added tabs component

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ import WPButton from './components/wp-button'
 import WPSpinner from './components/wp-spinner'
 import WPNotice from './components/wp-spinner'
 import WPMetabox from './components/wp-metabox'
+import {
+	WPTabs,
+	WPTabItem
+} from './components/wp-tabs'
 
 const components = {
 	WPButton,
 	WPSpinner,
 	WPNotice,
 	WPMetabox,
+	WPTabItem,
+	WPTabs,
 }
 
 components.install = (Vue, options = {}) => {
---
+++ stories/tabs.stories.js
@@ -0,0 +1,37 @@
+/* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
+
+import {
+	storiesOf
+} from '@storybook/vue';
+import {
+	action
+} from '@storybook/addon-actions';
+
+import {
+	WPTabs,
+	WPTabItem
+} from '../src/components/wp-tabs'
+
+storiesOf('Tabs', module)
+	.add('Tabs example', () => ({
+		components: {
+			WPTabs,
+			WPTabItem,
+		},
+		template:
+		`
+		<div>
+			<WPTabs>
+				<WPTabItem label="First tab">
+					<p>First tab content</p>
+				</WPTabItem>
+				<WPTabItem label="Second Tab">
+					<p>Second tab content</p>
+				</WPTabItem>
+			</WPTabs>
+		</div>
+		`,
+	}))
+	;
+
+/* eslint-enable react/react-in-jsx-scope */
